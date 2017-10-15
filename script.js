9f3a1be087b74ca3d51b1eb33a606ef7fe84154a
Cedric Hutchings <cedhut02@gmail.com>
2017-10-15 14:42:08 -0400

    Started working on title Page

---
+++ script.js
@@ -0,0 +1,120 @@
+var canvas;//  We'll define these
+var ctx;//	   guys later.
+
+var primaryColor   = '#005dff';
+var secondaryColor = '#81aaef';
+
+
+//Universal Functions. 
+//Generally speaking, the more simple the function, the higher up in the script it is.
+
+
+function adjustScreen(){
+
+	canvas.width  = $(window).width();//Get the screen's width to fill all available space.
+	canvas.height = $(window).height();//Get the screen's height to fill all available space.
+
+}
+
+
+function startScreen(){
+	var indexOfBigLetter = 0;
+	var bigLetterSizeBonus = 0;
+	var bigLetterSizeBonusChangeRate = 5;
+	const titleText = 'Rock Paper Scissors';
+
+
+	var paintGradient = function(){
+		var diagonalGradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+		//define bounds for gradient
+
+		diagonalGradient.addColorStop(0, primaryColor);//Define first color stop
+		diagonalGradient.addColorStop(1, secondaryColor);//Define second color stop
+
+		ctx.fillStyle = diagonalGradient;// 			   Set the gradient as our brush
+		ctx.fillRect(0, 0, canvas.width, canvas.height);// and slop it onto the screen.
+
+	};
+
+	var paintTitle = function(){
+		var fontSize = canvas.width/15;
+		ctx.font = fontSize + 'px Modak';//define font
+
+		var textDim = ctx.measureText(titleText);	 	//We'll need the text's dimensions to position it correctly.
+		textDim.x = canvas.width/2 - textDim.width/2;	//Here we calculate the x (center)
+		textDim.y = canvas.height/3;					//and the y (top 3rd of the page)
+
+		var textGradient = ctx.createLinearGradient(textDim.x, textDim.y, textDim.x+textDim.width, textDim.y);
+		//bounds for gradient
+
+		textGradient.addColorStop(0,   'dimgray');
+		textGradient.addColorStop(0.5, 'gray');
+		textGradient.addColorStop(1,   'dimgray');
+		//These three color stops should make the text appear kind of shiny.
+
+		var currentX = textDim.x;
+
+		ctx.fillStyle = textGradient;
+
+		for(var letterIndex = 0; letterIndex < titleText.length; letterIndex++){
+			var letter = titleText[letterIndex];
+
+			if(letterIndex == indexOfBigLetter){//If this guy is the big letter,
+				ctx.font = (fontSize + bigLetterSizeBonus) + 'px Modak';//Make the font size change
+				bigLetterSizeBonus = bigLetterSizeBonus + bigLetterSizeBonusChangeRate;
+				//Change the font size next turn.
+
+				if(bigLetterSizeBonus === 0 && bigLetterSizeBonusChangeRate < 0){//This passes the size bonus on to the next letter if it's gotten big and then smaller again.
+					bigLetterSizeBonusChangeRate = bigLetterSizeBonusChangeRate*-1;
+					indexOfBigLetter++;
+
+					if(indexOfBigLetter === titleText.length)indexOfBigLetter = 0;
+					//This sets it back to the beginning if it's gone through the entire title.
+				}
+
+				else if(bigLetterSizeBonus > 10)bigLetterSizeBonusChangeRate = bigLetterSizeBonusChangeRate * -1;
+				//Above line reverses direction of size increase
+
+				if(letter === ' ')ctx.font = fontSize + 'px Modak';
+				//This stops the increase of the font size if the letter is a space.
+			}
+
+			else ctx.font = fontSize + 'px Modak';//So the rest of the letters end up being the right size.
+
+			ctx.fillText(letter, currentX, textDim.y);
+
+			currentX = currentX + ctx.measureText(letter).width;
+		};
+		//ctx.strokeText(titleText, textDim.x, textDim.y);
+
+	};
+
+	var animateScreen = function(){
+		paintGradient();
+		paintTitle();
+
+		setTimeout(animateScreen, 50);
+
+	};
+
+	animateScreen();
+
+}
+
+//End of Universal Functions
+
+
+
+//Here'll be where we throw together all of our previously defined abstractions.
+$(document).ready(function(){
+	canvas = document.getElementById('gameCanvas');//Now that these guys have been loaded in,
+	ctx = canvas.getContext('2d');//				 we'll initialize them.
+
+	adjustScreen();
+
+	$(window).on('resize', function(){
+		adjustScreen();
+	});
+
+	startScreen();
+});
