29f44a35dbaa03933905bbd90b14d01ed6aae097
Cedric Hutchings <cedhut02@gmail.com>
2017-10-24 21:21:40 -0400

    Added chat

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/public'));  
 
 app.get('/', function(req, res,next){
-    res.sendFile(__dirname + '/node_modules/game.html');
+    res.sendFile(__dirname + '/public/game.html');
 });
 
 io.on('connection', function(client) {  
@@ -19,8 +19,8 @@ io.on('connection', function(client) {
 	});
 
 	client.on('messages', function(data) {//If we get a message, it comes with some data
-           client.emit('broad', data);//Send message back to the guy who sent it
-           client.broadcast.emit('broad',data);//Send message back to everyone else
+        client.emit('broad', data);//Send message back to the guy who sent it
+        client.broadcast.emit('broad',data);//Send message back to everyone else
     });
 
 });
